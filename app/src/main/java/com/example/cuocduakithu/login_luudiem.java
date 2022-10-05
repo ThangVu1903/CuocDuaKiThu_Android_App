cdd5348174e285b69619083bdc990a590cfc7895
ThangVu1903 <vuthang19032002az@gmail.com>
2022-10-05 14:21:44 +0700

    Remember Password

---
+++ app/src/main/java/com/example/cuocduakithu/login_luudiem.java
@@ -0,0 +1,14 @@
+package com.example.cuocduakithu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login_luudiem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_luudiem);
+    }
+}
